3025d626237f49b05cd99df167a98e57d2534fb4
Marcel Mantovani Filho <37183770+marcelmantovani@users.noreply.github.com>
2021-01-10 16:38:05 -0500

    Task 5 - part 1

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,10 +1,25 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file = “/home/marcel/.aws/credentials”
+  shared_credentials_file = "/home/marcel/.aws/credentials"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 
+module "t2micro_instance" {
+  source = "./modules/aws_ec2_instance"
+  instance_count = 4
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0de0caa8ec855eb50"]
+  subnet_id = "subnet-086d976e27bc71e25"
+  tags = {"Name" = "Udacity T2"}
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+module "t4large_instance" {
+  source = "./modules/aws_ec2_instance"
+  instance_count = 0
+  instance_type = "m4.large"
+  tags = {"Name" = "Udacity M4"}
+}
--- Exercise_1/modules/aws_ec2_instance/variables.tf
+++ Exercise_1/modules/aws_ec2_instance/variables.tf
@@ -1,11 +1,27 @@
 variable "instance_count"{
     description = "The number of instances required to be provisioned."
+    type = number
+    default = 1
 }
 
 variable "tags"{
     description = "Tags for your instances"
+    type = map(string)
+    default = {}
 }
 
 variable "instance_type" {
-    descript = "A valid AWS instance type, e.g. t2-micro, t4-large, etc."
+    description = "A valid AWS instance type, e.g. t2-micro, t4-large, etc."
+}
+
+variable "vpc_security_group_ids" {
+    description = "A list of security group IDs to associate with"
+    type = list
+    default = []
+}
+
+variable "subnet_id" {
+    description = "The VPC Subnet ID to launch in"
+    type = string
+    default = ""
 }
